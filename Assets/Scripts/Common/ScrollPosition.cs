6468ae7609a671817faff4092346db182c39a89e
chanseon park <hktrollgames@gmail.com>
2021-05-20 11:00:09 +0900

    Scripts 폴더 위치 이동

---
+++ Assets/Scripts/Common/ScrollPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollPosition : MonoBehaviour
+{
+    static public List<ScrollPosition> Items = new List<ScrollPosition>();
+
+    private void Awake()
+    {
+        //Debug.Log(name + "생성됨");
+        Items.Add(this);
+    }
+
+    private void Start()
+    {
+        speed *= GameManager.instance.scrollSpeedMultiply;
+    }
+
+    private void OnDestroy()
+    {
+        //Debug.Log(name + "삭제됨");
+        Items.Remove(this);
+    }
+
+
+    public float speed = -1f; // 1초에 한칸 왼쪽으로 이동.
+
+    void Update()
+    {
+        // 스크롤 가로 이동.
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
