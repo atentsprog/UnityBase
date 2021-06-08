94accd14efa1c9e0d2788e2495128da0dd6f13a4
hktrollgames <hktrollgames@gmail.com>
2021-06-08 19:51:14 +0900

    크래스 충돌되지 않도록 네임 스페이스 지정

--- Assets/UISample/GameManager.cs
+++ Assets/UISample/GameManager.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace UISample
 {
-    void Update()
+    public class GameManager : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        void Update()
         {
-            ChatUI.Instance.Show();
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ChatUI.Instance.Close();
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                ChatUI.Instance.Show();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                ChatUI.Instance.Close();
+            }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            QuestUI.Instance.Show();
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            QuestUI.Instance.Close();
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                QuestUI.Instance.Show();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                QuestUI.Instance.Close();
+            }
         }
     }
 }
