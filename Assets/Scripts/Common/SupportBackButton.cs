04f60ac0c245cfa5df852fc88389db765339e1d3
hktrollgames <hktrollgames@gmail.com>
2021-07-20 23:44:14 +0900

    SupportBackButton.cs 추가

---
+++ Assets/Scripts/Common/SupportBackButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupportBackButton : MonoBehaviour
+{
+    protected void OnEnable()
+    {
+        UIStackManager.PushUiStack(transform);
+    }
+
+    protected void OnDisable()
+    {
+        UIStackManager.PopUiStack(gameObject.GetInstanceID());
+    }
+}
