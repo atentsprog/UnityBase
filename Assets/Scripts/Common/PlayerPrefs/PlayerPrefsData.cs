7122b3bf803558b0fbd3ee1f677c3e7154a00df0
hktrollgames <hktrollgames@gmail.com>
2021-09-29 12:33:33 +0900

    사용안하는 Data 클래스 삭제

--- Assets/Scripts/Common/PlayerPrefs/PlayerPrefsData.cs
+++ Assets/Scripts/Common/PlayerPrefs/PlayerPrefsData.cs
@@ -2,12 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Data<T>
-{
-    public T data;
-}
-
 
 [System.Serializable]
 public class PlayerPrefsData<T> where T : new()
